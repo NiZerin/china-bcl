187a162c6207e74849b6f7ac29c4218b3d223d0d
Zeling Ning <nzl199851@gmail.com>
2020-06-12 15:08:43 +0800

    add sql file

--- resources/views/home/index/reset.blade.php
+++ resources/views/home/index/reset.blade.php
@@ -5,8 +5,6 @@
 <blockquote class="layui-elem-quote">
     请将 公司名称 或者 爆料id 发送到邮箱 <a href="mailto:nzl1998@aliyun.com">nzl1998@aliyun.com</a>
 </blockquote>
-
-
 <fieldset class="layui-elem-field layui-field-title" style="margin-top: 30px;">
     <legend>联系微信</legend>
 </fieldset>
